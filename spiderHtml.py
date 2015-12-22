89e806d785dccadff4d74764b4a59cf6961eaf9d
chenhao <chenhao1000@live.cn>
2015-12-22 13:58:45 +0800

    获取图片

--- spiderHtml.py
+++ spiderHtml.py
@@ -1,6 +1,4 @@
 # encoding:UTF-8
-import io
-import sys
 import re
 import urllib
 import urllib.request
@@ -10,7 +8,4 @@ req['word'] = '陈浩'
 url = "http://www.baidu.com/s?" + urllib.parse.urlencode(req)
 data = urllib.request.urlopen(url).read().decode('UTF-8')
 linkre = re.compile('href=\"(.+?)\"')
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='gb18030')
-for x in linkre.findall(data):
-    if 'http' in x:
-        print(x)
+print(data)
